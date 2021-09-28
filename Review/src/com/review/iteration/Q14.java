47445373343bd1d120b4a345ea32a228d84e341b
umingming <u_0112@naver.com>
2021-09-28 14:02:38 +0900

    과제

---
+++ Review/src/com/review/iteration/Q14.java
@@ -0,0 +1,20 @@
+package com.review.iteration;
+
+public class Q14 {
+
+	public static void main(String[] args) {
+		/*
+		최대 9자리의 정수를 입력 받아 각자리의 홀수 숫자합과 짝수 숫자함을 구하시오.
+		-9자리가 넘어가면 동작 금지
+		
+		설계>
+		1. BufferedReader
+		2. 숫자 입력
+		3. if문 9자리인지 확인 length메소드 사용
+			>while문 length까지
+				>
+		 */
+
+	}
+
+}
