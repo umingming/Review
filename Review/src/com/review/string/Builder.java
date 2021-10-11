adb04b874be829c65608725b79ab76c288521331
umingming <u_0112@naver.com>
2021-10-11 15:47:06 +0900

    StringBuilder

---
+++ Review/src/com/review/string/Builder.java
@@ -0,0 +1,13 @@
+package com.review.string;
+
+public class Builder {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("hi ");
+		sb.append("umi");
+		
+		System.out.println(sb.toString());
+
+	}
+
+}
