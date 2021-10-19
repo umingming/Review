e0342e9c9e07f3b840d978b52f42d099e5e9035d
umingming <u_0112@naver.com>
2021-10-19 15:49:52 +0900

    clear

--- Riview/src/com/java/question/file/Q09.java
+++ Riview/src/com/java/question/file/Q09.java
@@ -11,28 +11,43 @@ public class Q09 {
 		
 		설계>
 		1. 크기 정렬 디렉토리를 경로로 하는 File 객체 생성
-		2. 객체를 이용해 File 배열 선언
-		3. File을 매개로 하는 ArrayList 객체 생성
-		4. for문 배열 반복
-			>ArrayList에 배열의 요소 추가
-		5. for문 ArrayList의 크기 -1 반복
-			>for문 ArrayList의 크기 -1 -i 반복
-				>if j가 j+1번째 요소보다 큰지?
-					>temp File 생성해 j번째 요소 저장
-					>set메소드로 j번째에 j+1, j+1에 temp값 저장
+		2. File을 매개로 하는 ArrayList 객체 생성
+		3. addFile, bubbleFile 메소드 호출
+		4. addFile 메소드 
+			>File 객체로 File 배열 선언
+			>향상된 for문으로 배열 반복
+				>if file인가?
+					>ArrayList에 저장
+			>향상된 for문으로 배열 반복
+				>if dir인가?
+					>addFile 메소드 호출
+		5. bubbleFile메소드 
+			>for문 ArrayList의 크기 -1 반복
+				>for문 ArrayList의 크기 -1 -i 반복
+					>if j가 j+1번째 요소보다 큰지?
+						>temp File 생성해 j번째 요소 저장
+						>set메소드로 j번째에 j+1, j+1에 temp값 저장
 		6. for문 ArrayList 객체 반복
 			>파일명, 크기, 폴더 출력
 		 */
 		
 		File dir = new File("C:\\java\\file\\파일_디렉토리_문제\\크기 정렬");
-		File[] list = dir.listFiles();
-		
 		ArrayList<File> file = new ArrayList<File>();
 		
-		for(int i=0; i<list.length; i++) {
-			file.add(list[i]);
-		}
+		addFile(dir, file);
+		bubbleFile(file);
 		
+		System.out.println("[파일명]\t[크기]\t[파일이 들어있는 폴더]");
+		for(int i=0; i<file.size(); i++) {
+			System.out.printf("%s\t%.1fKB\t%s%n", 
+					file.get(i).getName(), 
+					((double)file.get(i).length()/1024), 
+					file.get(i).getParent().substring(file.get(i).getParent().lastIndexOf("\\")+1));
+		}
+
+	}
+
+	private static void bubbleFile(ArrayList<File> file) {
 		for(int i=0; i<file.size()-1; i++) {
 			for(int j=0; j<file.size()-1-i; j++) {
 				if(file.get(j).length() < file.get(j+1).length()) {
@@ -42,15 +57,19 @@ public class Q09 {
 				}
 			}
 		}
-		
-		for(int i=0; i<file.size(); i++) {
-			System.out.printf("%s\t%.1fKB\t%s%n", 
-					file.get(i).getName(), 
-					((double)file.get(i).length()/1024), 
-					file.get(i).getParent());
-		}
-		
-
 	}
 
+	private static void addFile(File dir, ArrayList<File> file) {
+		File[] list = dir.listFiles();
+		for(File subfile : list) {
+			if(subfile.isFile()) {
+				file.add(subfile);
+			}
+		}
+		for(File subdir : list) {
+			if(subdir.isDirectory()) {
+				addFile(subdir, file);
+			}
+		}
+	}
 }
