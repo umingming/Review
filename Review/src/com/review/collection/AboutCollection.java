62ea86a02555d6a625512a119e252d9752564370
umingming <u_0112@naver.com>
2021-10-21 20:31:49 +0900

    explain kind of collections

---
+++ Review/src/com/review/collection/AboutCollection.java
@@ -0,0 +1,29 @@
+package com.review.collection;
+
+public class AboutCollection {
+
+	public static void main(String[] args) {
+		
+		/*
+		Collection
+		-List와 Set의 inteface
+		List
+		-순서(index)가 있는 데이터 집합. 데이터의 값은 중복을 허용함.
+		-ArrayList; 요소 접근이 용이해, Shift시에 비용이 많이 소모
+			>배열의 번지를 객체가 가지고 있어 Shift가 발생하면 다시 배열을 만들어야 함.
+		-LinkedList; 요소 접근이 그렇게 용이하진 않아, Shift시에 비용 절약
+			>데이터 하나마다 방번호를 가지고 있어서 그게 하나하나 연결된 구조.		
+		-Vector; ArrayList와 비슷함
+			>같은 인터페이스를 공유하기 때문이지.
+		-Queue; 선입선출
+		-Stack; 후입선출
+		Set
+		-순서가 없는 데이터 집합. 데이터 값이 중복되지 않음.
+		Map
+		-순서가 없는 데이터 집합으로 key로 데이터 값을 구분함.
+		
+		 */
+
+	}
+
+}
