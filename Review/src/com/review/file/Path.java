92b4721490c2bacae85b5f791332f5b53ea30e75
umingming <u_0112@naver.com>
2021-10-23 22:38:39 +0900

    designating path

---
+++ Review/src/com/review/file/Path.java
@@ -0,0 +1,17 @@
+package com.review.file;
+
+public class Path {
+
+	public static void main(String[] args) {
+		String dirPath = "C:\\class\\java\\file\\파일_디렉토리_문제";
+		String Q04 = dirPath + "\\음악 파일";
+		String Q05 = dirPath + "\\확장자별 카운트";
+		String Q06 = dirPath + "\\파일 제거";
+		String Q07 = dirPath + "\\동일 파일";
+		String Q08 = dirPath + "\\폴더 삭제\\delete";
+		String Q09 = dirPath + "\\크기 정렬";
+		String Q10 = dirPath + "\\직원";
+
+	}
+
+}
---
+++ Review/src/com/review/file/Q04.java
@@ -0,0 +1,12 @@
+package com.review.file;
+
+public class Q04 {
+
+	public static void main(String[] args) {
+		/*
+		
+		 */
+
+	}
+
+}
