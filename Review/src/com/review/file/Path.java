099edff75a21ed2586f9e8544e969ee7bb8c7f08
umingming <u_0112@naver.com>
2021-10-23 22:43:36 +0900

    edit

--- Review/src/com/review/file/Path.java
+++ Review/src/com/review/file/Path.java
@@ -1,8 +1,6 @@
 package com.review.file;
 
 public class Path {
-
-	public static void main(String[] args) {
 		String dirPath = "C:\\class\\java\\file\\파일_디렉토리_문제";
 		String Q04 = dirPath + "\\음악 파일\\Music";
 		String Q05 = dirPath + "\\확장자별 카운트";
@@ -11,7 +9,5 @@ public class Path {
 		String Q08 = dirPath + "\\폴더 삭제\\delete";
 		String Q09 = dirPath + "\\크기 정렬";
 		String Q10 = dirPath + "\\직원";
-
-	}
 
 }
--- Review/src/com/review/file/Q04.java
+++ Review/src/com/review/file/Q04.java
@@ -1,11 +1,22 @@
 package com.review.file;
 
+import java.io.File;
+
 public class Q04 {
 
 	public static void main(String[] args) {
 		/*
+		음악 파일이 100개 있다. 파일명 앞에 일련 번호를 붙이시오.
 		
+		설계>
+		1. 음악 파일을 경로로 하는 File 객체 생성
+		2. file의 목록으로 File 배열 선언
+		3. 향상된 for문 배열 반복
+			>파일명을 출력할 때 일련 번호를 부여함.
 		 */
+		
+		File dir = new File(Path.Q01);
+		File[] files = dir.listFiles();
 
 	}
 
